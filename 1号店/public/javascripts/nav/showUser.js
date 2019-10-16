c7424a136e70cede017c8f4eee162b0692b5aa54
Qiao <3530078622@qq.com>
2019-10-16 11:03:08 +0800

    项目

---
+++ 1号店/public/javascripts/nav/showUser.js
@@ -0,0 +1,22 @@
+// console.log(document.cookie)
+
+$(function(){
+    get()
+})
+function get(){
+    var str = document.cookie
+    var arr = str.split(';')
+    var boo = false
+    for (var i=0;i<arr.length;i++) {
+        arr[i] = arr[i].trim().split('=') 
+        if (arr[i][0] === 'name') {
+            var user = JSON.parse(unescape(arr[i][1]))
+            $('#user').text(user.username)
+            boo=true
+        } 
+    }
+    if (!boo) {
+        location.href = '/admin/login'
+    }
+    console.log(arr)
+}
