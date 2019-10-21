58da45e8a505a271dd927c8d2ace815b0a679b53
Qiao <3530078622@qq.com>
2019-10-21 10:26:45 +0800

    123

--- 1号店/api/add.js
+++ 1号店/api/add.js
@@ -1,21 +1,21 @@
 const goods = require('../model/goods')
 
 function a(req,res){
-	// goods.create({
-	// 	type:"金币商城全部商品",
-	// 	img:"/images/han/diazi.jpg",
-	// 	miaoshu:"康尔 KingCamp 户外垫子睡垫防潮垫防污防水 露营野餐休闲防滑垫 超轻便携 210*200cm KG9001 彩虹条纹",
-	// 	price:89.0,
-	// 	golb:10,
-	// 	oldprice:118
-	// },{
-	// 	type:"金币商城全部商品",
-	// 	img:"https://m.360buyimg.com/babel/jfs/t1/58952/17/13739/218815/5da9746fE361df0db/a7f0c2c6988d400d.jpg",
-	// 	miaoshu:"CASSILE卡思乐女包卡包牛皮短款休闲零钱包轻薄时尚拉链卡包 桔红色",
-	// 	price:105.0,
-	// 	golb:10,
-	// 	oldprice:119
-	// })
+	goods.create({
+		type:"金币商城全部商品",
+		img:"/images/han/diazi.jpg",
+		miaoshu:"康尔 KingCamp 户外垫子睡垫防潮垫防污防水 露营野餐休闲防滑垫 超轻便携 210*200cm KG9001 彩虹条纹",
+		price:89.0,
+		golb:10,
+		oldprice:118
+	},{
+		type:"金币商城全部商品",
+		img:"https://m.360buyimg.com/babel/jfs/t1/58952/17/13739/218815/5da9746fE361df0db/a7f0c2c6988d400d.jpg",
+		miaoshu:"CASSILE卡思乐女包卡包牛皮短款休闲零钱包轻薄时尚拉链卡包 桔红色",
+		price:105.0,
+		golb:10,
+		oldprice:119
+	})
 	res.end('添加成功')
 }
 module.exports = a
--- 1号店/routes/coupons.js
+++ 1号店/routes/coupons.js
@@ -1,16 +1,29 @@
 const goods = require('../model/goods')
 function coupons(req,res){
+	var arr= []
 	goods.find({"type":"优惠分类"}).then((couponslist)=>{
-		for(var i=0;i<coupons.length;i++){
-			goods.find({"quantype":couponslist[i].couponslist}).then((result)=>{
-				res.render('coupons',{
-					i:i,
-					result:result,
-					couponslist:couponslist
-				})
+		// for(var i=0;i<couponslist.length;i++){
+		
+			
+			console.log(couponslist[0].couponslist)
+			// goods.find({"quantype":couponslist[0].couponslist}).then((result)=>{
+			// 	if(result){
+			// 		arr.push(result)
+					
+					
+			// 	}
+				
+				
+				
+			// })
+			res.render('coupons',{
+				// arr:arr,
+				// result:result,
+				// couponslist:couponslist
 			})
-		}
+		//}
 	})
+
 }
 
 module.exports = coupons
