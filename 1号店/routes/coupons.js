af3540173e4d9e155b8c4fdfcfc590eed686f865
Qiao <3530078622@qq.com>
2019-10-21 15:07:35 +0800

    123

--- 1号店/routes/coupons.js
+++ 1号店/routes/coupons.js
@@ -1,22 +1,11 @@
 const goods = require('../model/goods')
 function coupons(req,res){
-
-	var arr= []
 	goods.find({"type":"优惠分类"}).then((data)=>{
-		for(var i=0;i<data.length;i++){
-			goods.find({"quantype":data[i].couponslist}).then((quantype)=>{
-				arr.push(quantype)
-				
-				res.render('coupons',{
-					data : data,
-					result:arr
-				})
-			})
-		}
-		
-		
+		res.render('coupons',{
+			data:data
+		})
 	})
-	console.log(arr[0][0].price,'--------------------------')
+	
 }
 
 module.exports = coupons
