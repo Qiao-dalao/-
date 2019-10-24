ef6b69ca8a5ee3e60fa488a76fd7b2af5051374d
Qiao <3530078622@qq.com>
2019-10-24 17:24:42 +0800

    123

--- 1号店/routes/brands.js
+++ 1号店/routes/brands.js
@@ -1,9 +1,21 @@
 const goods = require('../model/goods.js')
+const users = require('../model/users.js')
 function brands(req,res){
-	goods.find({type:'超级品牌列表'}).then((results)=>{
-		res.render('brands',{
-			results:results
-		})
+	goods.find({type:'超级品牌列表'}).then(async (results)=>{
+		var str = req.cookies
+		var name = await users.find({'_id':str._id})
+		if(name.length==0){
+			res.render('brands',{
+				results,
+				name:[{name:'注册 登录'}]
+			}) 
+		}else{
+			res.render('brands',{
+				results,
+				name
+			}) 
+		}
+		
 	})
 }
 
--- 1号店/routes/items.js
+++ 1号店/routes/items.js
@@ -1,9 +1,21 @@
 const goods = require('../model/goods')
+const users = require('../model/users.js')
 function items(req,res){
-	goods.find({type:'超级单品抢购'}).then((result)=>{
-		res.render('items',{
-			results:result
-		})
+	goods.find({type:'超级单品抢购'}).then(async (results)=>{
+		var str = req.cookies
+		var name = await users.find({'_id':str._id})
+		if(name.length==0){
+			res.render('items',{
+				results,
+				name:[{name:'注册 登录'}]
+			}) 
+		}else{
+			res.render('items',{
+				results,
+				name
+			}) 
+		}
+		
 	})
 	
 }
